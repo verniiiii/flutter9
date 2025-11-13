11a18af0dbfa112ff7b82b365011b3eae1b83990
verniiiii <vera.shestopalova@gmail.com>
2025-11-13 20:57:05 +0300

    main.dart fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
 import 'core/app_router.dart';
-import 'features/transactions/data/transaction_store.dart';
+import 'features/transactions/data/transaction_model.dart';
+import 'features/transactions/data/transaction_form_store.dart';
+import 'features/transactions/data/edit_transaction_store.dart';
+import 'features/transactions/data/transactions_list_store.dart';
+import 'features/transactions/data/statistics_store.dart';
+import 'features/transactions/data/transaction_details_store.dart';
 
 void main() {
-  GetIt.I.registerSingleton<TransactionStore>(TransactionStore());
+  // Регистрируем общий ObservableList для всех stores
+  GetIt.I.registerSingleton<ObservableList<Transaction>>(ObservableList<Transaction>());
+
+  // Регистрируем все stores
+  GetIt.I.registerSingleton<TransactionFormStore>(TransactionFormStore());
+  GetIt.I.registerSingleton<EditTransactionStore>(EditTransactionStore());
+  GetIt.I.registerSingleton<TransactionsListStore>(TransactionsListStore());
+  GetIt.I.registerSingleton<StatisticsStore>(StatisticsStore());
+  GetIt.I.registerSingleton<TransactionDetailsStore>(TransactionDetailsStore());
+
   runApp(MyApp());
 }
 
